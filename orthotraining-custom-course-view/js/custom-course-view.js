64b50778d1dd582e128fa95f991e0e9fdf8e5523
Ken Sia <technical@threestepsbusiness.com>
2020-01-14 19:49:03 +0800

    Added post_grad_test custom field

--- orthotraining-custom-course-view/js/custom-course-view.js
+++ orthotraining-custom-course-view/js/custom-course-view.js
@@ -1,44 +1,133 @@
 (function( $ ) {
 	'use strict';
-     console.log('Module 1 date - ', moduleDates.module_1_date);
-	console.log('Module 2 date - ', moduleDates.module_2_date);
+	console.log('Module 1 date - ', moduleDates.module_1_date);
+	console.log('Module 2 date - ', moduleDates.module_2_1_date);
+	console.log('Module 2 date - ', moduleDates.module_2_2_date);
 	console.log('Module 3 date - ', moduleDates.module_3_date);
 	console.log('Module 4 date - ', moduleDates.module_4_date);
 	console.log('Module 5 date - ', moduleDates.module_5_date);
 	console.log('Module 6 date - ', moduleDates.module_6_date);
 	console.log('Module 7 date - ', moduleDates.module_7_date);
 	console.log('Module 8 date - ', moduleDates.module_8_date);
-	$('.post-2385').hide();
-	$('.post-2383').hide();
-	$('.post-2377').hide();
-	$('.post-2375').hide();
-	$('.post-2373').hide();
-	$('.post-2371').hide();
-	$('.post-2369').hide();
-	$('.post-2343').hide();
+	console.log('Module 9 date - ', moduleDates.module_9_date);
+	console.log('Smilefast PRO date - ', moduleDates.smilefast_pro_date);
+	console.log('PgD - ', moduleDates.post_grad_test);
 
+	var module_1_div = $("h2:contains('M1: Diagnosis & Treatment Planning')").parent().parent().parent();
+	var module_2_1_div = $("h2:contains('M2-1: Conventional Straight Wire Mechanics')").parent().parent().parent();
+	var module_2_2_div = $("h2:contains('M2-2: Conventional Straight Wire Mechanics')").parent().parent().parent();
+	var module_3_div = $("h2:contains('Early Treatment')").parent().parent().parent();
+	var module_4_div = $("h2:contains('Aligner Therapy')").parent().parent().parent();
+	var module_5_div = $("h2:contains('Aesthetic and Accelerated Orthodontics')").parent().parent().parent();
+	var module_6_div = $("h2:contains('Advanced Bio-mechanics')").parent().parent().parent();
+	var module_7_div = $("h2:contains('Management of CL 3 Malocclusions')").parent().parent().parent();
+	var module_8_div = $("h2:contains('IDT and Difficult Orthodontic Problems')").parent().parent().parent();
+	var module_9_div = $("h2:contains('PPS Module – Practice Team Training')").parent().parent().parent();
+// 	var smilefast_pro_div = $("h2:contains('Smilefast PRO')").parent().parent().parent();
+
+	var module_1_pgd_div = $("h2:contains('M1: Postgraduate Test')").parent().parent().parent();
+	var module_2_1_pgd_div = $("h2:contains('M2-1: Postgraduate Test')").parent().parent().parent();
+	var module_2_2_pgd_div = $("h2:contains('M2-2: Postgraduate Test')").parent().parent().parent();
+	var module_3_pgd_div = $("h2:contains('M3: Postgraduate Test')").parent().parent().parent();
+	var module_4_pgd_div = $("h2:contains('M4: Postgraduate Test')").parent().parent().parent();
+	var module_5_pgd_div = $("h2:contains('M5: Postgraduate Test')").parent().parent().parent();
+	var module_6_pgd_div = $("h2:contains('M6: Postgraduate Test')").parent().parent().parent();
+	var module_7_pgd_div = $("h2:contains('M7: Postgraduate Test')").parent().parent().parent();
+	var module_8_pgd_div = $("h2:contains('M8: Postgraduate Test')").parent().parent().parent();
+	var module_9_pgd_div = $("h2:contains('M9: Postgraduate Test')").parent().parent().parent();
+
+	module_1_div.hide();
+	module_2_1_div.hide();
+	module_2_2_div.hide();
+	module_3_div.hide();
+	module_4_div.hide();
+	module_5_div.hide();
+	module_6_div.hide();
+	module_7_div.hide();
+	module_8_div.hide();
+	module_9_div.hide();
+// 	smilefast_pro_div.hide();
+
+	module_1_pgd_div.hide();
+	module_2_1_pgd_div.hide();
+	module_2_2_pgd_div.hide();
+	module_3_pgd_div.hide();
+	module_4_pgd_div.hide();
+	module_5_pgd_div.hide();
+	module_6_pgd_div.hide();
+	module_7_pgd_div.hide();
+	module_8_pgd_div.hide();
+	module_9_pgd_div.hide();
+
+	$('#bb-course-list-grid-filters').hide();
+
+// 	if( strtotime('-42 day', strtotime('09/11/2019')) < strtotime('now') ) {
+// 	  echo 'show';
+// 	} else {
+// 	  echo 'hide';
+// 	}
 	if(moduleDates.module_1_date) {
-		$('.post-2343').show();
+		module_1_div.show();
+		if(moduleDates.post_grad_test) {
+			module_1_pgd_div.show();
+		}
 	}
-		if(moduleDates.module_2_date) {
-		$('.post-2369').show();
+	if(moduleDates.module_2_1_date) {
+		module_2_1_div.show();
+		if(moduleDates.post_grad_test) {
+			module_2_1_pgd_div.show();
+		}
 	}
-		if(moduleDates.module_3_date) {
-		$('.post-2371').show();
-	} 	if(moduleDates.module_4_date) {
-		$('.post-2373').show();
+	if(moduleDates.module_2_2_date) {
+		module_2_2_div.show();
+		if(moduleDates.post_grad_test) {
+			module_2_2_pgd_div.show();
+		}
 	}
-		if(moduleDates.module_5_date) {
-		$('.post-2375').show();
+	if(moduleDates.module_3_date) {
+		module_3_div.show();
+		if(moduleDates.post_grad_test) {
+			module_3_pgd_div.show();
+		}
 	}
-		if(moduleDates.module_6_date) {
-		$('.post-2377').show();
+	if(moduleDates.module_4_date) {
+		module_4_div.show();
+		if(moduleDates.post_grad_test) {
+			module_4_pgd_div.show();
+		}
 	}
-		if(moduleDates.module_7_date) {
-		$('.post-2383').show();
+	if(moduleDates.module_5_date) {
+		module_5_div.show();
+		if(moduleDates.post_grad_test) {
+			module_5_pgd_div.show();
+		}
 	}
-		if(moduleDates.module_8_date) {
-		$('.post-2385').show();
+	if(moduleDates.module_6_date) {
+		module_6_div.show();
+		if(moduleDates.post_grad_test) {
+			module_6_pgd_div.show();
+		}
 	}
+	if(moduleDates.module_7_date) {
+		module_7_div.show();
+		if(moduleDates.post_grad_test) {
+			module_7_pgd_div.show();
+		}
+	}
+	if(moduleDates.module_8_date) {
+		module_8_div.show();
+		if(moduleDates.post_grad_test) {
+			module_8_pgd_div.show();
+		}
+	}
+	if(moduleDates.module_9_date) {
+		module_9_div.show();
+		if(moduleDates.post_grad_test) {
+			module_9_pgd_div.show();
+		}
+	}
+// 		if(moduleDates.smilefast_pro_date) {
+// 		smilefast_pro_div.show();
+// 	}
 
 })( jQuery );
--- orthotraining-custom-course-view/orthotraining-custom-course-view.php
+++ orthotraining-custom-course-view/orthotraining-custom-course-view.php
@@ -2,41 +2,91 @@
   /**
   * Plugin Name: Orthotraining Custom Course View
   * Description: Custom plugin to show courses on specific registered dates
-  * Version: 1.0
+  * Version: 1.1
   */
 
-add_action( 'wp_loaded', 'get_module_dates' );
+
+add_action( 'parse_query', 'get_module_dates' );
+
 function get_module_dates() {
-  $userID = get_current_user_id();
-  $user = get_user_by( 'ID', $userID );
-  $module_1_date = get_user_meta( $user->id, 'module_1_date', true );
-	$module_2_date = get_user_meta( $user->id, 'module_2_date', true );
-	$module_3_date = get_user_meta( $user->id, 'module_3_date', true );
-	$module_4_date = get_user_meta( $user->id, 'module_4_date', true );
-	$module_5_date = get_user_meta( $user->id, 'module_5_date', true );
-	$module_6_date = get_user_meta( $user->id, 'module_6_date', true );
-	$module_7_date = get_user_meta( $user->id, 'module_7_date', true );
-	$module_8_date = get_user_meta( $user->id, 'module_8_date', true );
+	$page_id = get_queried_object_id();
+	if( $page_id == 2365) {
+	  $userID = get_current_user_id();
+	  $user = get_user_by( 'ID', $userID );
+	  $module_1_date = get_user_meta( $user->id, 'module_1_date', true );
+		$module_2_1_date = get_user_meta( $user->id, 'module_2_1_date', true );
+		$module_2_2_date = get_user_meta( $user->id, 'module_2_2_date', true );
+		$module_3_date = get_user_meta( $user->id, 'module_3_date', true );
+		$module_4_date = get_user_meta( $user->id, 'module_4_date', true );
+		$module_5_date = get_user_meta( $user->id, 'module_5_date', true );
+		$module_6_date = get_user_meta( $user->id, 'module_6_date', true );
+		$module_7_date = get_user_meta( $user->id, 'module_7_date', true );
+		$module_8_date = get_user_meta( $user->id, 'module_8_date', true );
+		$module_9_date = get_user_meta( $user->id, 'module_9_date', true );
+		$smilefast_pro_date = get_user_meta( $user->id, 'smilefast_pro_date', true );
+		$post_grad_test = get_user_meta( $user->id, 'post_grad_test', true );
 
-  wp_register_script(
-      'custom-course-view',
-	  	plugins_url( 'js/custom-course-view.js', __FILE__ ),
-       array( 'jquery' ),
-       1.0,
-       true
-  );
+// 	  if( strtotime('-42 day', strtotime($module_1_date)) > strtotime('now') ) {
+// 		$module_1_date = false;
+// 	  }
+	  if( strtotime('-42 day', strtotime($module_2_1_date)) > strtotime('now') ) {
+		$module_2_1_date = false;
+	  }
+	  if( strtotime('-42 day', strtotime($module_2_2_date)) > strtotime('now') ) {
+		$module_2_2_date = false;
+	  }
+	  if( strtotime('-42 day', strtotime($module_3_date)) > strtotime('now') ) {
+		$module_3_date = false;
+	  }
+	  if( strtotime('-42 day', strtotime($module_4_date)) > strtotime('now') ) {
+		$module_4_date = false;
+	  }
+	  if( strtotime('-42 day', strtotime($module_5_date)) > strtotime('now') ) {
+		$module_5_date = false;
+	  }
+	  if( strtotime('-42 day', strtotime($module_6_date)) > strtotime('now') ) {
+		$module_6_date = false;
+	  }
+	  if( strtotime('-42 day', strtotime($module_7_date)) > strtotime('now') ) {
+		$module_7_date = false;
+	  }
+	  if( strtotime('-42 day', strtotime($module_8_date)) > strtotime('now') ) {
+		$module_8_date = false;
+	  }
+	  if( strtotime('-42 day', strtotime($module_9_date)) > strtotime('now') ) {
+		$module_9_date = false;
+	  }
+	  if( strtotime('-42 day', strtotime($smilefast_pro_date)) > strtotime('now') ) {
+		$smilefast_pro_date = false;
+	  }
+	  if( $post_grad_test != 'Yes' ) {
+		$post_grad_test = false;
+	  }
 
-  wp_enqueue_script( 'custom-course-view' );
+	  wp_register_script(
+		  'custom-course-view',
+			plugins_url( 'js/custom-course-view.js', __FILE__ ),
+		   array( 'jquery' ),
+		   1.0,
+		   true
+	  );
 
-  wp_localize_script( 'custom-course-view', 'moduleDates', array(
-	  'module_1_date' => $module_1_date,
-	  'module_2_date' => $module_2_date,
-	  'module_3_date' => $module_3_date,
-	  'module_4_date' => $module_4_date,
-	  'module_5_date' => $module_5_date,
-	  'module_6_date' => $module_6_date,
-	  'module_7_date' => $module_7_date,
-	  'module_8_date' => $module_8_date
-  ) );
+	  wp_enqueue_script( 'custom-course-view' );
+
+	  wp_localize_script( 'custom-course-view', 'moduleDates', array(
+		  'module_1_date' => $module_1_date,
+		  'module_2_1_date' => $module_2_1_date,
+		  'module_2_2_date' => $module_2_2_date,
+		  'module_3_date' => $module_3_date,
+		  'module_4_date' => $module_4_date,
+		  'module_5_date' => $module_5_date,
+		  'module_6_date' => $module_6_date,
+		  'module_7_date' => $module_7_date,
+		  'module_8_date' => $module_8_date,
+		  'module_9_date' => $module_9_date,
+		  'smilefast_pro_date' => $smilefast_pro_date,
+		  'post_grad_test' => $post_grad_test
+	  ) );
+	}
 }
 ?>
